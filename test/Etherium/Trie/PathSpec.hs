c1191d503e6b645e3c24968be543b88e340e8f72
Ben Kirwin <ben@kirw.in>
2014-05-18 11:01:48 -0400

    Add common tests for path encoding / decoding
    Again, no bugs, but my test suite is getting a lot bigger...

--- test/Etherium/Trie/PathSpec.hs
+++ test/Etherium/Trie/PathSpec.hs
@@ -1,16 +1,31 @@
 {-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE DeriveGeneric #-}
 module Etherium.Trie.PathSpec(spec) where
 
+import Prelude hiding (seq)
+import Data.Aeson
 import qualified Data.ByteString as BS
 import Data.ByteString(ByteString)
 import Data.Functor
 import Data.Monoid
+import GHC.Generics
 
 import Etherium.Trie.Path
 
 import Test.Hspec
 import Test.QuickCheck
 import Etherium.Testing
+
+data HexCase = HexCase
+  { seq :: Path
+  , term :: Bool
+  , out :: ByteString
+  } deriving (Show, Generic)
+
+instance FromJSON Word4 where
+  parseJSON x = sndWord4 <$> parseJSON x
+
+instance FromJSON HexCase
 
 spec :: Spec
 spec = do
@@ -22,3 +37,11 @@ spec = do
 
   it "should recover the second nibble" $ property $ \x y ->
     (sndWord4 $ x `packByte` y) `shouldBe` y
+
+  describe "performs example conversions" $ testCommon "hexencodetest" $ \test -> do
+    it ("should encode to " ++ show (out test)) $
+      let encoded = encodePath (term test) (seq test)
+      in encoded `shouldBe` (out test)
+    it ("should decode " ++ show (out test)) $
+      let decoded = decodePath (out test)
+      in decoded `shouldBe` Just (term test, seq test)
