f1a925073dc6f01fe107561d872a72fc3ffccea1
Ben Kirwin <ben@kirw.in>
2014-05-23 09:55:03 -0400

    Unstub stubs slightly; fix contract root -> RLP
    A bit of tidying before dropping this code for the time being; it looks
    like the wiki is out of date, so I'll have to retouch all this anyways.

--- Etherium/Block.hs
+++ Etherium/Block.hs
@@ -3,8 +3,10 @@ module Etherium.Block() where
 
 import qualified Data.ByteString as BS
 import Data.ByteString(ByteString)
+import Data.Functor
 import GHC.Generics
 
+import qualified Etherium.RLP as RLP
 import Etherium.RLP.Convert
 import Etherium.Trie(Digest)
 
@@ -12,8 +14,8 @@ data Block = Block BlockHeader [Transaction] [Uncle] [StackFrame]
   deriving (Show, Generic)
 
 -- Stubs
-type Transaction = ByteString
-type Uncle = ByteString
+type Transaction = RLP.Item
+type Uncle = RLP.Item
 
 data BlockHeader = BlockHeader
   { parentHash :: ByteString
@@ -30,16 +32,25 @@ data BlockHeader = BlockHeader
   } deriving (Show, Generic)
 
 -- Stub
-data StackFrame = StackFrame ByteString ByteString
+data StackFrame = StackFrame RLP.Item RLP.Item
   deriving (Show, Generic)
 
 data Account = Account
   { accountBalance :: Integer
   , accountNonce :: Integer
-  , contractRoot :: Maybe Digest
+  , contractInfo :: ContractInfo
   } deriving (Show, Generic)
+
+data ContractInfo = ContractRoot Digest | External
+  deriving (Eq, Show)
 
 instance AsRLP Block
 instance AsRLP BlockHeader
 instance AsRLP StackFrame
 instance AsRLP Account
+
+instance AsRLP ContractInfo where
+  fromRLP (RLP.String bs) | BS.null bs = Just External
+  fromRLP item = ContractRoot <$> fromRLP item
+  toRLP (ContractRoot digest) = toRLP digest
+  toRLP External = RLP.String BS.empty
