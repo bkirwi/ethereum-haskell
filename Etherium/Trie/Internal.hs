48975b4431d3d5b0378a066a421e14812c229a28
Ben Kirwin <ben@kirw.in>
2014-05-24 16:49:28 -0400

    Create an 'Internal' module for Trie
    It's exposing a lot of implementation details for testing; splitting
    should help to define the public API.

---
+++ Etherium/Trie/Internal.hs
@@ -0,0 +1,159 @@
+{-# LANGUAGE FlexibleInstances #-}
+{-# LANGUAGE GeneralizedNewtypeDeriving #-}
+module Etherium.Trie.Internal
+  ( DB(..), Digest(..)
+  , getNode, putNode
+  , lookupPath
+  , insertRef, insertPath
+  , emptyRef
+  , Ref(..), Node(..)
+  ) where
+
+import qualified Crypto.Hash.SHA3 as SHA3
+import Control.Error
+import Control.Monad.State
+import qualified Data.ByteString as BS
+import Data.Char (intToDigit)
+import Data.List(stripPrefix)
+import qualified Data.Map as Map
+import Data.Map(Map)
+import qualified Data.Sequence as Seq
+import Data.Sequence(Seq)
+
+import Etherium.Prelude
+import qualified Etherium.RLP as RLP
+import Etherium.RLP.Convert
+import Etherium.Trie.Path
+
+newtype Digest = Digest ByteString
+  deriving (Ord, Eq, AsRLP)
+
+instance Show Digest where
+  show (Digest bs) = map (intToDigit . word4toInt) $ unpackWord4s bs 
+
+data Ref = Hash Digest | Literal Node
+  deriving (Show, Eq)
+
+data Node = Empty
+          | Shortcut Path (Either Ref ByteString)
+          | Full (Seq Ref) ByteString
+  deriving (Show, Eq)
+
+instance AsRLP Ref where
+  toRLP (Hash h) = toRLP h
+  toRLP (Literal l) = toRLP l
+  fromRLP rlp = do
+    bytes <- fromRLP rlp
+    if BS.length bytes < 32 
+      then hush (RLP.decode bytes) >>= fromRLP
+      else return . Hash . Digest $ bytes
+
+instance AsRLP Node where
+  toRLP Empty = toRLP BS.empty
+  toRLP (Shortcut path (Right val)) = toRLP [encodePath True path, val]
+  toRLP (Shortcut path (Left ref)) = toRLP [toRLP $ encodePath False path, toRLP ref]
+  toRLP (Full refs val) = toRLP (fmap toRLP refs <> Seq.singleton (toRLP val))
+  fromRLP (RLP.String bs)
+    | BS.null bs = Just Empty
+    | otherwise = Nothing
+  fromRLP (RLP.List [pathItem, targetItem]) = do
+    pathBS <- fromRLP pathItem
+    (isTerminal, path) <- decodePath pathBS
+    target <-
+      if isTerminal then Right <$> fromRLP targetItem 
+      else Left <$> fromRLP targetItem
+    return $ Shortcut path target 
+  fromRLP (RLP.List many) = do
+    guard $ length many == 17 
+    refs <- mapM fromRLP $ init many 
+    val <- fromRLP $ last many
+    return $ Full (Seq.fromList refs) val
+
+class (Functor m, Monad m) => DB m where
+  getDB :: Digest -> m Node
+  putDB :: Digest -> Node -> m ()
+
+putNode :: DB m => Node -> m Ref
+putNode node =
+  let bytes = RLP.encode $ toRLP node
+      digest = Digest $ SHA3.hash 256 bytes
+  in if BS.length bytes < 32
+    then return $ Literal node
+    else do
+      putDB digest node 
+      return $ Hash digest
+
+getNode :: DB m => Ref -> m Node
+getNode (Hash d) = getDB d
+getNode (Literal n) = return n
+            
+lookupPath :: DB m => Ref -> Path -> m ByteString
+lookupPath root path = getNode root >>= getVal
+  where
+    getVal Empty = return BS.empty
+    getVal (Shortcut nodePath (Right val)) = 
+      return $ case stripPrefix nodePath path of
+        Just [] -> val
+        _ -> BS.empty
+    getVal (Shortcut nodePath (Left ref)) =
+      case stripPrefix nodePath path of
+        Nothing -> return BS.empty
+        Just remaining -> lookupPath ref remaining
+    getVal (Full refs val) = case path of
+      [] -> return val
+      (w:rest) -> lookupPath (refs `Seq.index` word4toInt w) rest
+
+emptyRefs = Seq.replicate 16 $ Literal Empty
+emptyRef = Literal Empty
+
+toFull :: DB m => Node -> m Node
+toFull Empty = return $ Full emptyRefs BS.empty
+toFull f@(Full _ _) = return f
+toFull (Shortcut [] (Left ref)) = getNode ref >>= toFull
+toFull (Shortcut [] (Right bs)) = return $ Full emptyRefs bs
+toFull (Shortcut (p:ps) val) = do
+  ref <- putNode $ Shortcut ps val
+  let newRefs = Seq.update (word4toInt p) ref emptyRefs 
+  return $ Full newRefs BS.empty
+
+insertPath :: DB m => Node -> Path -> ByteString -> m Node
+insertPath Empty path bs 
+  | BS.null bs = return Empty
+  | otherwise = return $ Shortcut path $ Right bs
+insertPath (Shortcut nPath nVal) path bs = do
+  let (prefix, nSuffix, suffix) = splitPrefix nPath path
+  next <- case (nSuffix, suffix, nVal) of
+    ([], [], Right _) -> return $ Right bs
+    ([], _, Left ref) -> do
+      node <- getNode ref
+      newNode <- insertPath node suffix bs
+      return $ Left newNode 
+    _ -> do 
+      full <- toFull (Shortcut nSuffix nVal) 
+      newNode <- insertPath full suffix bs
+      return $ Left newNode
+  case (prefix, next) of
+    ([], Left newNode) -> return $ newNode
+    (_, Left newNode) -> Shortcut prefix . Left <$> putNode newNode 
+    (_, Right bs) -> return $ Shortcut prefix $ Right bs 
+  where
+    splitPrefix [] b = ([], [], b)  
+    splitPrefix a [] = ([], a, [])  
+    splitPrefix (a:as) (b:bs)
+      | a == b =
+        let (prefix, aSuffix, bSuffix) = splitPrefix as bs
+        in (a:prefix, aSuffix, bSuffix)
+      | otherwise = ([], a:as, b:bs)
+insertPath (Full refs val) [] bs = return $ Full refs bs 
+insertPath (Full refs val) (p:ps) bs = do
+  let index = word4toInt p
+      ref = refs `Seq.index` index
+  newRef <- insertRef ref ps bs
+  let newRefs = Seq.update index newRef refs
+  return $ Full newRefs val
+  
+insertRef :: DB m => Ref -> Path -> ByteString -> m Ref
+insertRef ref path bs = do
+  node <- getNode ref
+  newNode <- insertPath node path bs
+  putNode newNode
