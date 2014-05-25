75310c40c4f9495ed3099797501311cfeabf432d
Ben Kirwin <ben@kirw.in>
2014-05-25 17:21:06 -0400

    More docs; some block validations

--- Etherium/Block.hs
+++ Etherium/Block.hs
@@ -14,6 +14,8 @@ import Etherium.Trie(Digest)
 newtype Address = Address ByteString
   deriving (Show, AsRLP)
 
+type Timestamp = Integer
+
 data Block = Block 
   { blockHeader :: BlockHeader
   , uncleList :: [BlockHeader] 
@@ -26,14 +28,14 @@ data BlockHeader = BlockHeader
   , coinbaseAddress :: ByteString -- ^ Hb
   , blockStateRoot :: Digest      -- ^ Hr
   , transactionsRoot :: Digest    -- ^ Ht
-  , difficulty :: Integer         -- ^ Hd
+  , blockDifficulty :: Integer    -- ^ Hd
   , blockNumber :: Integer        -- ^ Hi
   , blockGasPrice :: Integer      -- ^ Hm
-  , blockGasLimit :: Integer
-  , blockGasUsed :: Integer
-  , timestamp :: Integer
-  , extraData :: ByteString
-  , blockNonce :: Digest
+  , blockGasLimit :: Integer      -- ^ Hl
+  , blockGasUsed :: Integer       -- ^ Hu
+  , blockTimestamp :: Timestamp   -- ^ Hs
+  , extraData :: ByteString       -- ^ Hx
+  , blockNonce :: Digest          -- ^ Hn
   } deriving (Show, Generic)
 
 data Account = Account
@@ -66,3 +68,34 @@ instance AsRLP BlockHeader
 instance AsRLP Account
 instance AsRLP Transaction
 instance AsRLP Receipt
+
+nextDifficulty :: BlockHeader
+              -> Timestamp
+              -> Integer
+nextDifficulty parent ts
+  | ts < blockTimestamp parent + 42 = previous + delta
+  | otherwise = previous - delta
+  where
+    previous = blockDifficulty parent
+    delta = previous `div` 1024
+  
+nextGasLimit :: BlockHeader -> Integer
+nextGasLimit parent = max limit 10000
+  where 
+    limit = (1023 * blockGasLimit parent + 6 * blockGasUsed parent `div` 5) `div` 1024
+
+proofOfWork :: BlockHeader
+            -> Integer
+proofOfWork = error "not implemented" -- yellow paper S 11.5
+
+isValidBlock :: BlockHeader
+             -> BlockHeader
+             -> Bool
+isValidBlock parent current =
+  let difficulty = blockDifficulty current
+      timestamp = blockTimestamp current
+  in proofOfWork current <= (2 ^ 256 `div` difficulty) &&
+     difficulty == nextDifficulty parent timestamp &&
+     blockGasLimit current == nextGasLimit parent &&
+     timestamp > blockTimestamp parent &&
+     BS.length (extraData current) <= 1024
