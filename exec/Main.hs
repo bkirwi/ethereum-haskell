7653a44d29abc551a2bbc2ae5b158e3f437b794c
Ben Kirwin <ben@kirw.in>
2014-05-31 23:49:11 -0400

    Another nonsensical main method
    This is a tiny TCP server that accepts connections from multiple hosts,
    first printing the socket address and then printing all the received
    input until the connection is closed.
    
    This uses pipes only because I'm hoping to use pipes-attoparsec later
    on, so I wanted to kick the tires. Pipes scares me a little but so far
    the obvious things have been working just fine.

--- exec/Main.hs
+++ exec/Main.hs
@@ -1,4 +1,17 @@
+{-# LANGUAGE OverloadedStrings #-}
 module Main (main) where
 
+import Pipes
+import qualified Pipes.Prelude as P
+import qualified Data.ByteString as BS
+import Pipes.Network.TCP
+
 main :: IO ()
-main = putStrLn "Hello, world!"
+main = withSocketsDo $ do
+  putStrLn "Hello, world!"
+  serve "*" "3030" handler
+  where
+    handler (socket, sockAddr) = do
+      putStrLn $ show sockAddr
+      let bytes = fromSocket socket 4096
+      runEffect $ for bytes $ \bs -> lift $ BS.putStr bs
